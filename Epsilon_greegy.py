920f4d6bedd15c26021eb993bd9314bcab8bf1b6
kentaro <kentaro.ishii@gmail.com>
2019-05-08 15:35:48 +0900

    copy complete.

--- Epsilon_greegy.py
+++ Epsilon_greegy.py
@@ -32,3 +32,58 @@ class Slot_Machine():
                 reward = 0.0
             self.toss_count += 1
             return reward, done
+
+class EpsilonGreedyAgent():
+
+    def __init__(self, epsilon):
+        self.epsilon = epsilon
+        self.V = []
+
+    def policy(self):
+        coins = range(len(self.V))
+        if random.random() < self.epcilone:
+            return random.choice(coins)
+        else:
+            return np.argmax(self.V)
+
+    def play(self, env):
+        # Initialize estimation.
+        N = [0] * len(env)
+
+        env.reset()
+        done = False
+        rewards = []
+        while not done:
+            selected_coin = self.policy()
+            reward, done = env.step(selected_coin)
+            rewards.append(reward)
+
+            n = N[selected_coin]
+            coin_average = (coin_average * n + reward) / (n + 1)
+            N[selected_coin] += 1
+            self.V[selected_coin] = new_average
+
+if __name__ == "__main__":
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    def main():
+        env = Slot_Machine([0.1,0.5,0.1,0.9,0.1])
+        epsilons = [0.0,0.1,0.2,0.5,0.8]
+        game_steps = list(range(10,310,10))
+        result = {}
+        for e in epsilons:
+            agent = EpsilonGreedyAgent(epsilon=e)
+            mean = []
+            for s in game_steps:
+                env.max_episode_steps = s
+                rewards = agent.play(env)
+                means.append(np.mean(rewards))
+            result["epsilone={}".format(e)] = means
+        result["coin toss count"] = game_steps
+        result = pd.DataFrame(result)
+        result.set_index("coin toss count", drop=True, inplace=True)
+        result.plot.line(figsize(10,5))
+        plt.show()
+
+main()
